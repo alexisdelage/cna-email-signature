110e05be25ff9dfd35a7dcf693d0fd851b2b7221
Alexis Delage <alexidelage@gmail.com>
2025-05-24 11:25:12 +0200

    chore: configure linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettier,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -23,6 +31,16 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'linebreak-style': ['error', 'unix'],
+      'prettier/prettier': ['error', { usePrettierrc: true }],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'react/no-array-index-key': 'error',
+      'react-hooks/exhaustive-deps': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
-)
+);
