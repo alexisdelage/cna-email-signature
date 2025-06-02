00eef04120aa72d0c893c939b2bf22917390f87b
Alexis Delage <alexis.delage@theodo.com>
2025-06-02 21:09:21 +0200

    feat: update default content

--- src/emailSignatureTemplate/defaultContent.ts
+++ src/emailSignatureTemplate/defaultContent.ts
@@ -1,8 +1,8 @@
 export const defaultContent = `# Cassandra LUMNI
-*Promotion 2020*
+*Commission Jeunes*
 ---
-Commission Jeunes
 Chercheuse en biologie marine à IFREMER
+Promotion E2020
 ---
 Tel : +33 X XX XX XX XX
 cassandra.lumni@centraliens-nantes.org
