e5071ab3a53fcc45ecdca27a99689cf83f564fe6
Alexis Delage <alexis.delage@theodo.com>
2026-06-13 19:50:54 +0200

    chore: remove advertisement

--- src/emailSignatureTemplate/SignatureTemplate.tsx
+++ src/emailSignatureTemplate/SignatureTemplate.tsx
@@ -8,7 +8,6 @@ import { Row } from '@react-email/row';
 
 import { buildAbsoluteUrl } from './urls';
 import logoAlumniRelativePath from '/logo_cna_small.png';
-import advertisementImageRelativePath from '/trophees-cna-2.webp';
 
 const emailRegex = /\b[\w.!#$%&'*+/=?^_`{|}~-]+@[\w.-]+\b/g;
 const phoneRegex = /(\+\s?)?\b\d[\d\s.-]{8,}\d\b/g;
@@ -35,88 +34,68 @@ export function SignatureTemplate({ markdownContent }: SignatureTemplateProps) {
   const content = prettify(markdownContent);
 
   const logoAlumniUrl = buildAbsoluteUrl(logoAlumniRelativePath);
-  const advertisementImageUrl = buildAbsoluteUrl(
-    advertisementImageRelativePath,
-  );
   const link = buildAbsoluteUrl('https://www.centraliens-nantes.org');
 
   const logoWidth = 140;
 
   return (
-    <Column>
-      <Row>
-        <Column valign="top" width={logoWidth + 20 + (7 + 19)}>
-          <Row>
-            <Column width={logoWidth + 20}>
-              <Link href={link} rel="noopener noreferrer">
-                <Img
-                  src={logoAlumniUrl} // do not use relative path in emails
-                  alt="Centrale Nantes Alumni"
-                  width={logoWidth}
-                  style={{
-                    ...fontStyles,
-                    marginRight: 20,
-                    marginBottom: 0,
-                  }}
-                />
-              </Link>
-            </Column>
-            <Column valign="middle" width={7 + 19}>
-              <div
+    <Row>
+      <Column valign="top" width={logoWidth + 20 + (7 + 19)}>
+        <Row>
+          <Column width={logoWidth + 20}>
+            <Link href={link} rel="noopener noreferrer">
+              <Img
+                src={logoAlumniUrl} // do not use relative path in emails
+                alt="Centrale Nantes Alumni"
+                width={logoWidth}
                 style={{
-                  width: 4,
-                  height: 41,
-                  backgroundColor: '#FAB600',
-                  marginRight: 19,
+                  ...fontStyles,
+                  marginRight: 20,
+                  marginBottom: 0,
                 }}
               />
-            </Column>
-          </Row>
-        </Column>
-        <Column valign="top">
-          <Markdown
-            markdownCustomStyles={{
-              p: {
-                ...fontStyles,
-              },
-              h1: {
-                ...fontStyles,
-                fontSize: 'medium',
-                fontWeight: 'bold',
-              },
-              hr: {
-                width: 25,
-                height: 4,
+            </Link>
+          </Column>
+          <Column valign="middle" width={7 + 19}>
+            <div
+              style={{
+                width: 4,
+                height: 41,
                 backgroundColor: '#FAB600',
-                border: 'none',
-                marginLeft: 0,
-              },
-              link: {
-                color: 'inherit',
-                textDecoration: 'none',
-              },
-            }}
-          >
-            {content}
-          </Markdown>
-        </Column>
-      </Row>
-
-      <Link
-        href="https://www.centraliens-nantes.org/fr/event/trophy-soiree-de-remise-des-trophees-alumni-nantraliens-aux-laureates-trophy/2026/04/01/4150"
-        rel="noopener noreferrer"
-      >
-        <Img
-          src={advertisementImageUrl} // do not use relative path in emails
-          alt="A venir pour 2026 : les Trophées Alumni Nantraliens !"
-          width={450}
-          style={{
-            ...fontStyles,
-            marginTop: 20,
+                marginRight: 19,
+              }}
+            />
+          </Column>
+        </Row>
+      </Column>
+      <Column valign="top">
+        <Markdown
+          markdownCustomStyles={{
+            p: {
+              ...fontStyles,
+            },
+            h1: {
+              ...fontStyles,
+              fontSize: 'medium',
+              fontWeight: 'bold',
+            },
+            hr: {
+              width: 25,
+              height: 4,
+              backgroundColor: '#FAB600',
+              border: 'none',
+              marginLeft: 0,
+            },
+            link: {
+              color: 'inherit',
+              textDecoration: 'none',
+            },
           }}
-        />
-      </Link>
-    </Column>
+        >
+          {content}
+        </Markdown>
+      </Column>
+    </Row>
   );
 }
 
